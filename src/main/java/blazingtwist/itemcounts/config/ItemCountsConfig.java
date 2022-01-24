100a83d57bfbe63f191d178591fc8d79a9e06c3c
BlazingTwist <39350649+BlazingTwist@users.noreply.github.com>
2022-01-24 21:19:26 +0100

    re-work config to support offset from cursor and hotbar independently

--- src/main/java/blazingtwist/itemcounts/config/ItemCountsConfig.java
+++ src/main/java/blazingtwist/itemcounts/config/ItemCountsConfig.java
@@ -6,59 +6,70 @@ import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = "itemcounts")
 public class ItemCountsConfig implements ConfigData {
-	@ConfigEntry.Category("category.itemcounts")
-	public CountDisplayOption showActiveItemCount = CountDisplayOption.ALWAYS;
-	@ConfigEntry.Category("category.itemcounts")
-	public DurabilityItemOption showActiveItemDurabilityFilter = DurabilityItemOption.GOLD_DIAMOND_NETHERITE;
-	@ConfigEntry.Category("category.itemcounts")
-	public DurabilityDisplayOption showActiveItemDurability = DurabilityDisplayOption.ALWAYS;
-	@ConfigEntry.Category("category.itemcounts")
-	public float activeItemCountTextScale = 0.5f;
-	@ConfigEntry.Category("category.itemcounts")
-	public IconDisplayOption showActiveItemIcon = IconDisplayOption.ALWAYS;
-	@ConfigEntry.Category("category.itemcounts")
-	@ConfigEntry.Gui.CollapsibleObject
-	public HudOffset activeItemCountOffset = new HudOffset(-10, 10);
+	@ConfigEntry.Category("mainHand")
+	@ConfigEntry.Gui.CollapsibleObject(startExpanded = true)
+	public ItemRenderConfig mainHand_relativeToHotbarConfig = new ItemRenderConfig(false, CountDisplayOption.ALWAYS,
+			DurabilityItemOption.GOLD_DIAMOND_NETHERITE, DurabilityDisplayOption.ALWAYS, IconDisplayOption.ALWAYS,
+			0, -5, 0.75f);
+	@ConfigEntry.Category("mainHand")
+	@ConfigEntry.Gui.CollapsibleObject(startExpanded = true)
+	public ItemRenderConfig mainHand_relativeToCrosshairConfig = new ItemRenderConfig(true, CountDisplayOption.ALWAYS,
+			DurabilityItemOption.GOLD_DIAMOND_NETHERITE, DurabilityDisplayOption.ALWAYS, IconDisplayOption.ALWAYS,
+			-10, 10, 0.5f);
 
-	@ConfigEntry.Category("category.itemcounts")
-	public CountDisplayOption showOffhandItemCount = CountDisplayOption.ALWAYS;
-	@ConfigEntry.Category("category.itemcounts")
-	public DurabilityItemOption showOffhandItemDurabilityFilter = DurabilityItemOption.GOLD_DIAMOND_NETHERITE;
-	@ConfigEntry.Category("category.itemcounts")
-	public DurabilityDisplayOption showOffhandItemDurability = DurabilityDisplayOption.ALWAYS;
-	@ConfigEntry.Category("category.itemcounts")
-	public float offhandItemCountTextScale = 0.5f;
-	@ConfigEntry.Category("category.itemcounts")
-	public IconDisplayOption showOffhandItemIcon = IconDisplayOption.ALWAYS;
-	@ConfigEntry.Category("category.itemcounts")
-	@ConfigEntry.Gui.CollapsibleObject
-	public HudOffset offhandItemCountOffset = new HudOffset(10, 10);
+	@ConfigEntry.Category("offHand")
+	@ConfigEntry.Gui.CollapsibleObject(startExpanded = true)
+	public ItemRenderConfig offHand_relativeToHotbarConfig = new ItemRenderConfig(false, CountDisplayOption.ALWAYS,
+			DurabilityItemOption.GOLD_DIAMOND_NETHERITE, DurabilityDisplayOption.ALWAYS, IconDisplayOption.ALWAYS,
+			0, -5, 0.75f);
+	@ConfigEntry.Category("offHand")
+	@ConfigEntry.Gui.CollapsibleObject(startExpanded = true)
+	public ItemRenderConfig offHand_relativeToCrosshairConfig = new ItemRenderConfig(true, CountDisplayOption.ALWAYS,
+			DurabilityItemOption.GOLD_DIAMOND_NETHERITE, DurabilityDisplayOption.ALWAYS, IconDisplayOption.ALWAYS,
+			10, 10, 0.5f);
 
+	@ConfigEntry.Category("hotbar")
+	@ConfigEntry.Gui.CollapsibleObject(startExpanded = true)
+	public ItemRenderConfig hotbar_relativeToHotbarConfig = new ItemRenderConfig(true, CountDisplayOption.ALWAYS,
+			DurabilityItemOption.GOLD_DIAMOND_NETHERITE, DurabilityDisplayOption.ALWAYS, IconDisplayOption.NEVER,
+			0, -5, 0.75f);
 
-	@ConfigEntry.Category("category.itemcounts")
-	public CountDisplayOption showOnHotbar = CountDisplayOption.ALWAYS;
-	@ConfigEntry.Category("category.itemcounts")
-	public DurabilityItemOption showHotbarItemDurabilityFilter = DurabilityItemOption.GOLD_DIAMOND_NETHERITE;
-	@ConfigEntry.Category("category.itemcounts")
-	public DurabilityDisplayOption showHotbarItemDurability = DurabilityDisplayOption.ALWAYS;
-	@ConfigEntry.Category("category.itemcounts")
-	public float hotbarItemCountTextScale = 0.75f;
-	@ConfigEntry.Category("category.itemcounts")
-	public IconDisplayOption showHotbarItemIcon = IconDisplayOption.NEVER;
-	@ConfigEntry.Category("category.itemcounts")
-	@ConfigEntry.Gui.CollapsibleObject
-	public HudOffset hotbarItemCountOffset = new HudOffset(0, -5);
+	public static class ItemRenderConfig {
+		public ItemRenderConfig() {
+		}
+
+		public ItemRenderConfig(boolean enabled, CountDisplayOption countOption,
+								DurabilityItemOption durabilityFilter, DurabilityDisplayOption durabilityOption,
+								IconDisplayOption iconOption,
+								int xOffset, int yOffset, float textScale) {
+			this.countOption = countOption;
+			this.durabilityFilter = durabilityFilter;
+			this.durabilityOption = durabilityOption;
+			this.iconOption = iconOption;
+			offset = new HudOffset(xOffset, yOffset, textScale);
+		}
+
+		public boolean enabled = false;
+		public CountDisplayOption countOption = CountDisplayOption.NEVER;
+		public DurabilityItemOption durabilityFilter = DurabilityItemOption.NONE;
+		public DurabilityDisplayOption durabilityOption = DurabilityDisplayOption.NEVER;
+		public IconDisplayOption iconOption = IconDisplayOption.NEVER;
+		@ConfigEntry.Gui.CollapsibleObject()
+		public HudOffset offset = new HudOffset(0, -5, 0.75f);
+	}
 
 	public static class HudOffset {
 		public HudOffset() {
 		}
 
-		public HudOffset(int x, int y) {
+		public HudOffset(int x, int y, float textScale) {
 			this.x = x;
 			this.y = y;
+			this.textScale = textScale;
 		}
 
 		public int x = 0;
 		public int y = 0;
+		public float textScale = 1f;
 	}
 }
