f10a5d5d5f6334bb7b06db66f5533890e4783641
BlazingTwist <blazingtwist1@gmail.com>
2023-01-08 10:22:43 +0100

    implement issue #3 - separate counts by nbt, item name and durability

--- src/main/java/blazingtwist/itemcounts/config/ItemCountsConfig.java
+++ src/main/java/blazingtwist/itemcounts/config/ItemCountsConfig.java
@@ -3,36 +3,81 @@ package blazingtwist.itemcounts.config;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 
 @Config(name = "itemcounts")
 public class ItemCountsConfig implements ConfigData {
 	@ConfigEntry.Category("mainHand")
 	@ConfigEntry.Gui.CollapsibleObject(startExpanded = true)
-	public ItemRenderConfig mainHand_relativeToHotbarConfig = new ItemRenderConfig(false, CountDisplayOption.ALWAYS,
-			DurabilityItemOption.GOLD_DIAMOND_NETHERITE, DurabilityDisplayOption.ALWAYS, IconDisplayOption.ALWAYS,
-			0, -5, 0.75f);
+	public ItemRenderConfig mainHand_relativeToHotbarConfig = new ItemRenderConfig(
+			false,
+			CountDisplayOption.ALWAYS,
+			DurabilityItemOption.GOLD_DIAMOND_NETHERITE,
+			DurabilityDisplayOption.ALWAYS,
+			IconDisplayOption.ALWAYS,
+			0,
+			-5,
+			0.75f
+	);
 	@ConfigEntry.Category("mainHand")
 	@ConfigEntry.Gui.CollapsibleObject(startExpanded = true)
-	public ItemRenderConfig mainHand_relativeToCrosshairConfig = new ItemRenderConfig(true, CountDisplayOption.ALWAYS,
-			DurabilityItemOption.GOLD_DIAMOND_NETHERITE, DurabilityDisplayOption.ALWAYS, IconDisplayOption.ALWAYS,
-			-10, 10, 0.5f);
+	public ItemRenderConfig mainHand_relativeToCrosshairConfig = new ItemRenderConfig(
+			true,
+			CountDisplayOption.ALWAYS,
+			DurabilityItemOption.GOLD_DIAMOND_NETHERITE,
+			DurabilityDisplayOption.ALWAYS,
+			IconDisplayOption.ALWAYS,
+			-10,
+			10,
+			0.5f
+	);
 
 	@ConfigEntry.Category("offHand")
 	@ConfigEntry.Gui.CollapsibleObject(startExpanded = true)
-	public ItemRenderConfig offHand_relativeToHotbarConfig = new ItemRenderConfig(false, CountDisplayOption.ALWAYS,
-			DurabilityItemOption.GOLD_DIAMOND_NETHERITE, DurabilityDisplayOption.ALWAYS, IconDisplayOption.ALWAYS,
-			0, -5, 0.75f);
+	public ItemRenderConfig offHand_relativeToHotbarConfig = new ItemRenderConfig(
+			false,
+			CountDisplayOption.ALWAYS,
+			DurabilityItemOption.GOLD_DIAMOND_NETHERITE,
+			DurabilityDisplayOption.ALWAYS,
+			IconDisplayOption.ALWAYS,
+			0,
+			-5,
+			0.75f
+	);
 	@ConfigEntry.Category("offHand")
 	@ConfigEntry.Gui.CollapsibleObject(startExpanded = true)
-	public ItemRenderConfig offHand_relativeToCrosshairConfig = new ItemRenderConfig(true, CountDisplayOption.ALWAYS,
-			DurabilityItemOption.GOLD_DIAMOND_NETHERITE, DurabilityDisplayOption.ALWAYS, IconDisplayOption.ALWAYS,
-			10, 10, 0.5f);
+	public ItemRenderConfig offHand_relativeToCrosshairConfig = new ItemRenderConfig(
+			true,
+			CountDisplayOption.ALWAYS,
+			DurabilityItemOption.GOLD_DIAMOND_NETHERITE,
+			DurabilityDisplayOption.ALWAYS,
+			IconDisplayOption.ALWAYS,
+			10,
+			10,
+			0.5f
+	);
 
 	@ConfigEntry.Category("hotbar")
 	@ConfigEntry.Gui.CollapsibleObject(startExpanded = true)
-	public ItemRenderConfig hotbar_relativeToHotbarConfig = new ItemRenderConfig(true, CountDisplayOption.ALWAYS,
-			DurabilityItemOption.GOLD_DIAMOND_NETHERITE, DurabilityDisplayOption.ALWAYS, IconDisplayOption.NEVER,
-			0, -5, 0.75f);
+	public ItemRenderConfig hotbar_relativeToHotbarConfig = new ItemRenderConfig(
+			true,
+			CountDisplayOption.ALWAYS,
+			DurabilityItemOption.GOLD_DIAMOND_NETHERITE,
+			DurabilityDisplayOption.ALWAYS,
+			IconDisplayOption.NEVER,
+			0,
+			-5,
+			0.75f
+	);
+
+	@ConfigEntry.Category("rules")
+	@ConfigEntry.Gui.CollapsibleObject(startExpanded = true)
+	public ItemCountSeparationRules item_count_rules = new ItemCountSeparationRules(
+			false,
+			false,
+			false
+	);
 
 	public static class ItemRenderConfig {
 		@AutoConfigConstructor
@@ -74,5 +119,46 @@ public class ItemCountsConfig implements ConfigData {
 		public int x = 0;
 		public int y = 0;
 		public float textScale = 1f;
+	}
+
+	public static class ItemCountSeparationRules {
+		@AutoConfigConstructor
+		public ItemCountSeparationRules() {
+		}
+
+		public ItemCountSeparationRules(boolean separateMetadata, boolean separateName, boolean separateDurability) {
+			this.separateNbtData = separateMetadata;
+			this.separateName = separateName;
+			this.separateDurability = separateDurability;
+		}
+
+		public boolean separateNbtData = false;
+		public boolean separateName = false;
+		public boolean separateDurability = false;
+
+		public boolean countStacksSeparately(ItemStack a, ItemStack b) {
+			return (separateNbtData && !nbtDataMatches(a, b))
+					|| (separateName && !nameMatches(a, b))
+					|| (separateDurability && !durabilityMatches(a, b));
+		}
+
+		private boolean nbtDataMatches(ItemStack a, ItemStack b) {
+			if (a.hasNbt() != b.hasNbt()) {
+				return false;
+			}
+			if(!a.hasNbt() || a.getNbt() == null) {
+				// neither itemStack has NBT data
+				return true;
+			}
+			return a.getNbt().equals(b.getNbt());
+		}
+
+		private boolean nameMatches(ItemStack a, ItemStack b) {
+			return a.getName().equals(b.getName());
+		}
+
+		private boolean durabilityMatches(ItemStack a, ItemStack b) {
+			return a.getDamage() == b.getDamage();
+		}
 	}
 }
